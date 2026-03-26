0a23e0f2ea458f1420d2cc8c424c3a17661c003e
omprakashpatna18-del <om.prakashpatna18@gmail.com>
2026-03-26 21:06:32 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -225,12 +225,27 @@ Follow the given format:
 5. Overall Feedback
 """
 
+    try:
     response = gemini_model.models.generate_content(
         model="gemini-2.0-flash",
         contents=prompt
+    )
+    suggestions = response.text
+
+except Exception as e:
+    print("Gemini error:", e)
+
+    # 🔥 fallback (VERY IMPORTANT)
+    suggestions = f"""
+1. Strengths: Your current performance shows potential.
+2. Areas to be Improved: Focus more on weak areas.
+3. Precautions: Avoid inconsistency in study routine.
+4. Suggestions: Maintain a balanced study plan.
+5. Overall Feedback: You can improve with consistent effort.
+"""
     
           
           
     )
     return { "predicted_marks": marks,
-        "suggestions": response.text}
+        "suggestions": suggestions}
